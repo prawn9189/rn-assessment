8ccdf05bb3dba9fb9bf96a17998f2ed0776e4031
prawn9189 <prawn9189@hotmail.com>
2024-12-29 00:55:16 +0700

    feat[Routes]: Add default component for non existing routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,29 @@
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 
-export default function RootLayout() {
-  return <Stack />;
+// Prevent splash screen auto hiding
+SplashScreen.preventAutoHideAsync();
+
+function RootLayout() {
+  useEffect(() => {
+    SplashScreen.hideAsync();
+  }, []);
+
+  return (
+    <ThemeProvider value={DefaultTheme}>
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{ headerShown: false, title: "Appointments" }}
+        />
+        <Stack.Screen name="+not-found" />
+      </Stack>
+      <StatusBar style="inverted" />
+    </ThemeProvider>
+  );
 }
+
+export default RootLayout;
