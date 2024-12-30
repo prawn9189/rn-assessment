30e2c3ea30857dd254f6f15a2675bc42919b21d0
prawn9189 <prawn9189@hotmail.com>
2024-12-30 22:00:50 +0700

    refactor[Nav]: Create conditional navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,76 @@
 import FeatherIcons from "@expo/vector-icons/Feather";
-import { Tabs } from "expo-router";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { useRouter } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+
+import { checkLogin, logout } from "@/scripts/database";
+
+import Appointments from "./index";
+import NewAppointment from "./new-appointment";
+import Reviews from "./reviews";
+
+const Tab = createBottomTabNavigator();
 
 function TabLayout() {
+  const [authUser, setAuthUser] = useState<string>();
+
+  const router = useRouter();
+
+  const getUser = useCallback(async () => {
+    const result = await checkLogin();
+    setAuthUser(result);
+  }, []);
+
+  useEffect(() => {
+    getUser();
+  }, [getUser]);
+
+  const logoutUser = async () => {
+    await logout();
+    router.replace("/login");
+  };
+
   return (
-    <Tabs>
-      <Tabs.Screen
+    <Tab.Navigator>
+      <Tab.Screen
+        component={Appointments}
         name="index"
         options={{
           tabBarIcon: ({ color }) => (
             <FeatherIcons color={color} name="calendar" size={28} />
           ),
+          headerShown: true,
+          headerRight: () => (
+            <FeatherIcons onPress={logoutUser} name="log-out" size={28} />
+          ),
           title: "Appointments",
         }}
       />
-      <Tabs.Screen
-        name="new-appointment"
-        options={{
-          tabBarIcon: ({ color }) => (
-            <FeatherIcons color={color} name="plus" size={28} />
-          ),
-          title: "Create New",
-        }}
-      />
-    </Tabs>
+      {authUser === "user" && (
+        <Tab.Screen
+          component={NewAppointment}
+          name="new-appointment"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <FeatherIcons color={color} name="plus" size={28} />
+            ),
+            title: "Create New",
+          }}
+        />
+      )}
+      {authUser === "admin" && (
+        <Tab.Screen
+          component={Reviews}
+          name="reviews"
+          options={{
+            tabBarIcon: ({ color }) => (
+              <FeatherIcons color={color} name="plus" size={28} />
+            ),
+            title: "Reviews",
+          }}
+        />
+      )}
+    </Tab.Navigator>
   );
 }
 
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,34 @@
+import { Redirect } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
+import { checkLogin, initDB, seedData } from "@/scripts/database";
+
+SplashScreen.preventAutoHideAsync();
+
 function Appointments() {
+  const [authUser, setAuthUser] = useState<string>();
+
+  const init = useCallback(async () => {
+    await initDB();
+    await seedData();
+  }, []);
+
+  const getAuthUser = useCallback(async () => {
+    const result = await checkLogin();
+    setAuthUser(result);
+  }, []);
+
+  useEffect(() => {
+    init();
+    getAuthUser();
+  }, []);
+
+  if (authUser === "") {
+    return <Redirect href="/login" />;
+  }
+
   return (
     <View>
       <Text>Appointments</Text>
