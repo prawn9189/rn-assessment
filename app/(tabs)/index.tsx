c8245f7415250806d677259d72c8c516abffc034
prawn9189 <prawn9189@hotmail.com>
2024-12-31 16:37:26 +0700

    feat[Appt]: List appointments

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,17 @@
 import { Redirect, useSegments } from "expo-router";
 import { useCallback, useEffect, useState } from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Text } from "@/components/ui/text";
 import { checkLogin, getALlAppts, initDB, seedData } from "@/scripts/database";
 
 import type { Appt } from "@/scripts/database";
@@ -42,9 +52,27 @@ function Appointments() {
     return <Redirect href="/login" />;
   }
 
+  const viewAppt = () => {};
+
   return (
     <View>
-      <Text>Appointments</Text>
+      {appts.length !== 0 &&
+        appts.map((appt, index) => (
+          <Card className="bg-zinc-800 mb-4 mx-4" key={index}>
+            <CardHeader>
+              <CardTitle>{appt.patient}</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-row justify-between">
+              <Text className="w-1/2 text-zinc-400">Reason</Text>
+              <Text className="w-1/2">{appt.reason}</Text>
+            </CardContent>
+            <CardFooter>
+              <Button className="w-full" onPress={viewAppt}>
+                <Text>View</Text>
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
     </View>
   );
 }
